67b555b141d14e42cf38874b4c111325a0854f06
Varshath Akula <varshathakula05@gmail.com>
2024-08-17 23:13:43 +0530

    Changed Models to APIs

--- app.py
+++ app.py
@@ -2,50 +2,53 @@ import streamlit as st
 from flair.data import Sentence
 from flair.models import SequenceTagger
 
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-import torch.nn.functional as F
+from transformers import AutoTokenizer
+import requests
 
-
-
-ckpt_path="amphora/FinABSA"
+auth_token = "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"
+MaskedLM_ckpt='roberta-large'
 NER_tag_list = ['ORG']
-@st.cache_resource
-def load_model():
-    return AutoModelForSeq2SeqLM.from_pretrained(ckpt_path)
-ABSA = load_model()
-tokenizer = AutoTokenizer.from_pretrained(ckpt_path)
 tagger = SequenceTagger.load('ner')
+MaskedLM_tok = AutoTokenizer.from_pretrained(MaskedLM_ckpt)
+headers = {"Authorization": f"Bearer {auth_token}"}
 
-def run_single_absa(input_str, tgt):
-    input_str = input_str.replace(tgt, '[TGT]')
-    input = tokenizer(input_str, return_tensors='pt')
+API_URL_MLM = "https://api-inference.huggingface.co/models/FacebookAI/roberta-large"
+def query_mlm(payload):
+	response = requests.post(API_URL_MLM, headers=headers, json=payload)
+	return response.json()
 
-    output = ABSA.generate(
-        **input,
-        max_length=20,
-        output_scores=True,
-        return_dict_in_generate=True
-    )
+API_URL_CLF = "https://api-inference.huggingface.co/models/ProsusAI/finbert"
+def query_clf(payload):
+	response = requests.post(API_URL_CLF, headers=headers, json=payload)
+	return response.json()
 
-    classification_output = tokenizer.convert_ids_to_tokens(
-        int(output['sequences'][0][-4])
-    )
-    logits = F.softmax(output['scores'][-4][:, -3:], dim=1)[0]
-
-    return {
-        "classification_output": classification_output,
-        "logits":
-            {
-                'positive': float(logits[0]),
-                'negative': float(logits[1]),
-                'neutral': float(logits[2])
-            }
-    }
 def retrieve_target(input_str):
     sentence = Sentence(input_str)
     tagger.predict(sentence)
     entities = [entity.text for entity in sentence.get_spans('ner') if entity.tag in NER_tag_list]
     return entities
+def prompt_sentiment_analysis(input_str):
+    input_for_clf = input_str.split('<sep>')[1].strip()
+    clf_output = query_clf({
+        "inputs": input_for_clf,
+        })
+    return clf_output
+
+def prompt_mask_filling(input_str,tgt):
+    prompt = "Given this [TGT] shares are likely to [MSK]."
+    prompt = prompt.replace('[TGT]',tgt).replace('[MSK]',MaskedLM_tok.mask_token)
+    input_str = input_str + '<sep>' + prompt
+    query_mlm_output = query_mlm({
+        "inputs": input_str,
+        })
+    max_output = max(query_mlm_output , key = lambda x:x['score'])
+    return max_output['sequence']
+
+def run_single_absa(input_str,tgt):
+    mlm_output = prompt_mask_filling(input_str,tgt)
+    clf_output = prompt_sentiment_analysis(mlm_output)
+    return max(clf_output[0] , key = lambda x:x['score'])
+
 def run_absa(input_str):
     tgt_entities = retrieve_target(input_str)
     output = {e: run_single_absa(input_str, e) for e in tgt_entities}
@@ -57,4 +60,4 @@ button = st.button("Analyze")
 if user_input and button:
     final_output = run_absa(user_input)
     for Entity_Name in final_output:
-        st.write("Company : ",Entity_Name,", Sentiment : ",final_output[Entity_Name]['classification_output'],", Score : ",final_output[Entity_Name]['logits'][final_output[Entity_Name]['classification_output'].lower()])
+        st.write("Company : ",Entity_Name,", Sentiment : ",final_output[Entity_Name]['label'].upper(),", Score : ",final_output[Entity_Name]['score'])
